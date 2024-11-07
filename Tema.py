d3575ef7eb207f65d300f17a9f42f55927a0a69e
kuribohbolado <kuribohbolado@gmail.com>
2024-11-07 15:26:48 -0300

    Update Tema.py

--- Tema.py
+++ Tema.py
@@ -1,3 +1,4 @@
 #Um navio cargueiro radioativo naufragou na costa e temos que criar um programa de criptografia para enviar uma mensagem sobre a situação atual.
 
 #Temos que fazer um progama que traduza as mensagens nesta situação específicas.
+print("tchca tchaca na buthca")
