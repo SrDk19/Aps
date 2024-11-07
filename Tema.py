9280d047ea70a688e0c75f3389ecec03bb33745f
kuribohbolado <kuribohbolado@gmail.com>
2024-11-07 15:57:37 -0300

    Update Tema.py
    finalmente fiz a poha da codificação basica

--- Tema.py
+++ Tema.py
@@ -1,10 +1,8 @@
 #Um navio cargueiro radioativo naufragou na costa e temos que criar um programa de criptografia para enviar uma mensagem sobre a situação atual.
 
 #Temos que fazer um progama que traduza as mensagens nesta situação específicas.
-
-
-
-mensagem=[]
-
-receber=input("escreva a mensagem que sera criptografada")
-print(ord(receber))
+senha=input("escreva uma mensagem")
+cod=""
+for i in senha:
+    cod= cod + chr(ord(i)+10)
+print(cod)
