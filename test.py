4856afd11231b82d93cb0250aa34ca9d1a028d67
SrDk19 <jvalvessouza2005@gmail.com>
2024-11-07 17:13:01 -0300

    Revert "Att do test"
    This reverts commit a78d457ad81616cdf38cb811a6f3884e5a219aed.

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 import os
-import time
-
-#Temos que fazer um progama que traduza as mensagens nesta situação específicas.
 
 def erase():  # Limpa o terminal
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -11,14 +8,10 @@ def erase():  # Limpa o terminal
     elif os.name == 'posix': #windows
         os.system('clear')
 
-senha=input("escreva uma mensagem: ")
-cod2=[]
+senha=input("Escreva uma mensagem:")
 cod=""
-recor=""
 for i in senha:
-    cod= cod + chr(ord(i)+10)
-for i in cod:
-    recor=recor + chr(ord(i)-10)
+    cod = cod + chr(ord(i)+10)
 print(cod)
-print(recor)
-
+decod = cod - chr(ord(i)+10)   
+print(decod)
